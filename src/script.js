f620256d2e2f5afc040e58a0b203a21f96edd635
Anmol Sah <kumarxxx242424@gmail.com>
2024-03-30 14:50:33 +0530

    modified script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,6 @@
 document.getElementById('mobile-menu-button').addEventListener('click',function(){
     document.getElementById('mobile-menu').classList.toggle('hidden');
 })
-document.addEventListener("DOMContentLoaded", function () {
-    const charts = document.querySelectorAll(".chart");
-    charts.forEach(chart => {
-        const percentage = parseInt(chart.getAttribute("data-percent")); 
-        chart.style.width = percentage + "%";
-    });
-});
 
  // Function to scroll to the top of the page smoothly
  function scrollToTop() {
