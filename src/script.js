04405afdaa6e1bb2e900855e792fa7f9b41c3dad
Anmol Sah <kumarxxx242424@gmail.com>
2024-04-05 01:45:27 +0530

    final commit

--- src/script.js
+++ src/script.js
@@ -6,7 +6,8 @@ document.getElementById('mobile-menu-button').addEventListener('click',function(
  function scrollToTop() {
     window.scrollTo({
         top: 0,
-        behavior: 'smooth'
+        behavior: 'smooth',
+        
     });
 }
 
@@ -15,22 +16,3 @@ document.getElementById('switch-mode').addEventListener('click', function() {
     document.body.classList.toggle('light-mode');
 });
 
-
-
-  
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
- 
-  const firebaseConfig = {
-    apiKey: "AIzaSyChWA8pJMA_QdJG-Jph0i0iWeZZiaJJZus",
-    authDomain: "anmolsahportfolio.firebaseapp.com",
-    projectId: "anmolsahportfolio",
-    storageBucket: "anmolsahportfolio.appspot.com",
-    messagingSenderId: "432687667525",
-    appId: "1:432687667525:web:c134ec4c65d5bbeb6d462c",
-    measurementId: "G-J8Q2BJ9NFY"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
